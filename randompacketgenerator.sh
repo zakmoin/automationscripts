d9aa7906921ba8e7d8dbc86438cf254451ff9bb3
zaki <zaki.1moinuddin@gmail.com>
2018-07-24 22:40:54 +0100

    TarfficGenerator

---
+++ randompacketgenerator.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+VAR1="$(pip list --format=columns | grep "^ScapyTrafficGenerator*" | cut -d ' ' -f 1)"
+if [ "$VAR1" == "ScapyTrafficGenerator" ]; then
+	ScapyTrafficGenerator -X http -r '-X 1 -E 3 -F /home/devbox1/testdata -i enp0s3 -s 192.168.0.1 -S 10000 -d 192.168.0.100 -D 80 -m 12:31:25:15:a1:55 -M 12:31:25:15:a1:2b' 1>/dev/null
+	ScapyTrafficGenerator -X http -r '-X 1 -F /home/devbox1/testdata -i enp0s3 -s 192.168.0.1 -S 10000 -d 192.168.0.100 -D 80 -m 12:31:25:15:a1:55 -M 12:31:25:15:a1:2b' 1>/dev/null
+else
+	echo "No Installated"
+fi
+
